60e81761bf64eea41dd199c347861af349301b7e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-11 15:57:28 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,74 @@
 class UsersController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
+  # GET /users
+  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+  # GET /users/1
+  # GET /users/1.json
   def show
   end
 
-  # GET /posts/new
+  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /posts/1/edit
+  # GET /users/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+  # POST /users
+  # POST /users.json
   def create
-    @user = User.new(post_params)
+    @user = User.new(user_params)
 
     respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+  # DELETE /users/1
+  # DELETE /users/1.json
   def destroy
-    @post.destroy
+    @user.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
+    def set_user
+      @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:name)
+    def user_params
+      params.require(:user).permit(:name)
     end
 end
